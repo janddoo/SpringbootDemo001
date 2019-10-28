78f800f9446297e908a5d9e75a3521c865985ad4
andyCumt <1538132446@qq.comgit config --global user.name andyCumt;git config --global user.email 1538132446@qq.com>
2019-10-28 15:33:54 +0800

    这是一个新的项目,验证Springboot

---
+++ springBoot-mybatis-demo0001/src/main/java/com/niit/SpringBootMybatisDemo0001Application.java
@@ -0,0 +1,13 @@
+package com.niit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisDemo0001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisDemo0001Application.class, args);
+	}
+
+}
